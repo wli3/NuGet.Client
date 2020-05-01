cec23495e00462a78f89028b6d691ccb43a9161f
Kartheek Penagamuri <52756182+kartheekp-ms@users.noreply.github.com>
2020-04-30 19:15:23 -0700

    refactored XmlUtility in NuGet.Packaging project

--- build/Shared/XmlUtility.cs
+++ build/Shared/XmlUtility.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.IO;
 using System.Xml;
 using System.Xml.Linq;
 
@@ -12,16 +13,24 @@ namespace NuGet.Shared
         /// <summary>
         /// Creates a new System.Xml.Linq.XDocument from a file.
         /// </summary>
-        /// <param name="filePath">A URI string that references the file to load into a new <see cref="System.Xml.Linq.XDocument"/></param>
+        /// <param name="inputUri">A URI string that references the file to load into a new <see cref="System.Xml.Linq.XDocument"/></param>
         /// <returns>An <see cref="System.Xml.Linq.XDocument"/> that contains the contents of the specified file.</returns>
-        public static XDocument Load(string filePath)
+        internal static XDocument Load(string inputUri)
         {
-            if (string.IsNullOrEmpty(filePath))
+            using (var reader = XmlReader.Create(inputUri, GetXmlReaderSettings()))
             {
-                throw new ArgumentNullException(nameof(filePath));
+                return XDocument.Load(reader);
             }
+        }
 
-            using (var reader = XmlReader.Create(filePath, GetXmlReaderSettings()))
+        /// <summary>
+        /// Creates a new System.Xml.Linq.XDocument from a stream.
+        /// </summary>
+        /// <param name="input">The stream that contains the XML data.</param>
+        /// <returns>An <see cref="System.Xml.Linq.XDocument"/> that contains the contents of the specified stream.</returns>
+        internal static XDocument Load(Stream input)
+        {
+            using (var reader = XmlReader.Create(input, GetXmlReaderSettings()))
             {
                 return XDocument.Load(reader);
             }
